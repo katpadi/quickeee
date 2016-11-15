3d428977646f970f5d7526fcc03a79759fbc27a5
Kat Padilla <hello@katpadi.ph>
2016-11-16 04:00:18 +0800

    Complete crud

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -17,6 +17,30 @@ class ChatRoomsController < ApplicationController
     end
   end
 
+  def edit
+    @chat_room = current_user.chat_rooms.find_by(id: params[:id])
+  end
+
+  def update
+    @chat_room = current_user.chat_rooms.find_by(id: params[:id])
+    @chat_room.update(chat_room_params)
+    if @chat_room.update(chat_room_params)
+      flash[:success] = 'Chat room updated'
+      redirect_to @chat_room
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @chat_room = current_user.chat_rooms.find_by(id: params[:id])
+    @chat_room.destroy
+    if @chat_room.save
+      flash[:success] = 'Chat room deleted'
+    end
+    redirect_to chat_rooms_path
+  end
+
   def show
     @chat_room = ChatRoom.includes(:messages).find_by(id: params[:id])
     @message = Message.new
