919d55d4b2b6c353dc80ed40cb924002cc374f47
Kat Padilla <hello@katpadi.ph>
2016-11-16 01:05:31 +0800

    Heroku production stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,9 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = "wss://quickeee.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://quickeee.herokuapp.com',
+                                                 'http://quickeee.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
